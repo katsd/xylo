4df99af53ca81a62e4716eb112e7ade09607e25f
Katsu Matsuda <kats.mtd@gmail.com>
2019-12-31 01:09:34 +0900

    Update vm_test.cpp

--- tests/vm_test.cpp
+++ tests/vm_test.cpp
@@ -6,8 +6,16 @@
 //
 
 #include <stdio.h>
+#include "../src/vm/vm.hpp"
 
 int main()
 {
+    std::vector<unsigned long> iseq = {VM::PUSH_CONST, 0, VM::OUT, VM::END};
+    std::vector<VM::Obj> const_table = {VM::Obj((long)3)};
+
+    VM eval = VM(iseq, const_table);
+
+    eval.Run();
+
     return 0;
 }
