0424ed77fd4d612126fca9e772b6c27d1307161b
Katsu Matsuda <kats.mtd@gmail.com>
2019-12-31 17:38:38 +0900

    Clear main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,8 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <memory>
 
 int main()
 {
-    std::unique_ptr<int[]> p(new int[10]);
-
-    p[0] = 1;
-    p[1] = 2;
-    p[2] = 8;
-
-    printf("%d %d %d\n", p[0], p[1], p[2]);
-
-    int *np = (int *)realloc(p.get(), 20 * sizeof(int));
-
-    p.release();
-    p.reset(np);
-
-    p[19] = 3;
-
-    printf("%d %d %d\n", p[0], p[1], p[2]);
-    printf("%d\n", p[19]);
-
     return 0;
 }
