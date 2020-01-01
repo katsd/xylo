9257d4ef9bbc93137ebae887de555512c1496dab
Katsu Matsuda <kats.mtd@gmail.com>
2020-01-01 20:59:29 +0900

    Add token.hpp

---
+++ src/token.hpp
@@ -0,0 +1,94 @@
+//
+// token.hpp
+// xylo
+//
+// Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+#ifndef TOKEN_HPP_
+#define TOKEN_HPP_
+
+#include <string>
+
+enum Reserved
+{
+    FUNC,
+    RETURN,
+    REPEAT,
+    FOR,
+    WHILE,
+    IF,
+    ELSE,
+};
+
+enum Symbol
+{
+    PLUS,     // +
+    MINUS,    // -
+    MUL,      // *
+    DIV,      // /
+    BAND,     // &
+    BOR,      //|
+    BXOR,     // ^
+    BNOT,     // ~
+    AND,      // &&
+    OR,       // ||
+    NOT,      // !
+    EQUAL,    // ==
+    ASSIGN,   // =
+    QUE,      // ?
+    COMMMA,   // ,
+    DOT,      // .
+    LPAREN,   // (
+    RPAREN,   // )
+    LBRACKET, // {
+    RBRACKET, // }
+
+};
+
+enum TokenType
+{
+    RESERVED,
+    SYMBOL,
+    CONST,
+    OTHER,
+};
+
+union TokenVal {
+    Reserved reserved;
+    Symbol symbol;
+    unsigned long val;
+
+    TokenVal() {}
+
+    TokenVal(Reserved reserved)
+    {
+        this->reserved = reserved;
+    }
+
+    TokenVal(Symbol symbol)
+    {
+        this->symbol = symbol;
+    }
+
+    TokenVal(unsigned long val)
+    {
+        this->val = val;
+    }
+};
+
+struct Token
+{
+    TokenType type;
+    TokenVal token;
+    std::string str;
+
+    Token(TokenType type, TokenVal token, std::string str)
+    {
+        this->type = type;
+        this->token = token;
+        this->str = str;
+    }
+};
+
+#endif /* TOKEN_HPP_ */
