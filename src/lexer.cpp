4210849a020e53b1b777893af429ecb148323061
Katsu Matsuda <kats.mtd@gmail.com>
2020-01-01 18:08:31 +0900

    Implement Lexer::RemoveComment()

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,3 +4,52 @@
 //
 // Copyright (c) 2020 Katsu Matsuda. All rights reserved.
 //
+
+#include "lexer.hpp"
+
+std::string Lexer::RemoveComment(std::string code_str)
+{
+    std::string new_code_str = "";
+
+    unsigned long code_str_size = code_str.size();
+
+    unsigned long index = 0;
+
+    while (index < code_str_size)
+    {
+        if (index + 1 < code_str_size && code_str[index] == '/')
+        {
+            if (code_str[index + 1] == '/')
+            {
+                while (index < code_str_size && !(0 <= index - 1 && code_str[index - 1] == '/n'))
+                    index += 1;
+            }
+
+            if (code_str[index + 1] == '*')
+            {
+                while (index < code_str_size && !(0 <= index - 2 && code_str[index - 2] == '*' && code_str[index - 1] == '/'))
+                    index += 1;
+            }
+        }
+
+        if (index < code_str_size)
+            new_code_str += code_str[index];
+    }
+
+    return new_code_str;
+}
+
+Lexer::Result Lexer::Tokenize()
+{
+    code = std::vector<std::string>();
+
+    code_str = RemoveComment(code_str);
+
+    puts(code_str.c_str());
+
+    std::string current = "";
+
+    unsigned long index = 0;
+
+    return Result(true, code);
+}
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -8,8 +8,38 @@
 #ifndef LEXER_CPP_
 #define LEXER_CPP_
 
+#include <stdio.h>
+#include <string>
+#include <vector>
+
 class Lexer
 {
+private:
+    std::string code_str;
+
+    std::vector<std::string> code;
+
+    std::string RemoveComment(std::string code_str);
+
+public:
+    struct Result
+    {
+        bool success;
+        std::vector<std::string> code;
+
+        Result(bool success, std::vector<std::string> code)
+        {
+            this->success = success;
+            this->code = code;
+        }
+    };
+
+    Result Tokenize();
+
+    Lexer(std::string code_str)
+    {
+        this->code_str = code_str;
+    }
 };
 
 #endif /* LEXER_CPP_ */
