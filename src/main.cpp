2f07f97c457cb75cdedc4a3a005e2fa1b7e44c37
Katsu Matsuda <kats.mtd@gmail.com>
2020-01-01 18:18:39 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,27 @@
 //
 
 #include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main()
+#include "xylo.hpp"
+
+int main(int args, char *argv[])
 {
+    std::fstream ifs(argv[1]);
+
+    if (!ifs)
+    {
+        printf("failed to open file");
+        return 0;
+    }
+
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    ifs.close();
+
+    std::string code_str(ss.str());
+
     return 0;
 }
