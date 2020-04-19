4daca6c76ea36525a86609fdd3e598e282c69605
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-20 00:11:34 +0900

    Add Token::Symbol2Str

--- include/Token/Token.hpp
+++ include/Token/Token.hpp
@@ -13,97 +13,101 @@
 
 namespace xylo
 {
-	struct Token
+struct Token
+{
+ private:
+	TokenValue value;
+
+	std::string source;
+
+ public:
+	TokenType type;
+
+	SourcePos pos;
+
+	[[nodiscard]]
+	inline uint64_t GetInt() const
 	{
-	 private:
-		TokenValue value;
+		return value.ival;
+	}
 
-		std::string source;
+	[[nodiscard]]
+	inline double GetFloat() const
+	{
+		return value.dval;
+	}
 
-	 public:
-		TokenType type;
+	[[nodiscard]]
+	inline std::string GetString() const
+	{
+		return source;
+	}
 
-		SourcePos pos;
+	[[nodiscard]]
+	inline Symbol GetSymbol() const
+	{
+		return value.symbol;
+	}
 
-		[[nodiscard]]
-		inline uint64_t GetInt() const
+	[[nodiscard]]
+	inline Reserved GetReserved() const
+	{
+		return value.reserved;
+	}
+
+	[[nodiscard]]
+	inline std::string GetName() const
+	{
+		return source;
+	}
+
+	inline void Set(int64_t ival, const std::string& src)
+	{
+		type = TokenType::INT;
+		value = ival;
+	}
+
+	inline void Set(double dval, const std::string& src)
+	{
+		type = TokenType::FLOAT;
+		value = dval;
+	}
+
+	// "**" -> STRING
+	// **** -> NAME
+	inline void Set(const std::string& src)
+	{
+		if (!src.empty() && src[0] == '"' && src[src.size() - 1] == '"')
 		{
-			return value.ival;
+			type = TokenType::STRING;
+			source = src.substr(1, src.size() - 2);
 		}
-
-		[[nodiscard]]
-		inline double GetFloat() const
+		else
 		{
-			return value.dval;
-		}
-
-		[[nodiscard]]
-		inline std::string GetString() const
-		{
-			return source;
-		}
-
-		[[nodiscard]]
-		inline Symbol GetSymbol() const
-		{
-			return value.symbol;
-		}
-
-		[[nodiscard]]
-		inline Reserved GetReserved() const
-		{
-			return value.reserved;
-		}
-
-		[[nodiscard]]
-		inline std::string GetName() const
-		{
-			return source;
-		}
-
-		inline void Set(int64_t ival, const std::string& src)
-		{
-			type = TokenType::INT;
-			value = ival;
-		}
-
-		inline void Set(double dval, const std::string& src)
-		{
-			type = TokenType::FLOAT;
-			value = dval;
-		}
-
-		// "**" -> STRING
-		// **** -> NAME
-		inline void Set(const std::string& src)
-		{
-			if (!src.empty() && src[0] == '"' && src[src.size() - 1] == '"')
-			{
-				type = TokenType::STRING;
-				source = src.substr(1, src.size() - 2);
-			}
-			else
-			{
-				type = TokenType::NAME;
-				source = src;
-			}
-		}
-
-		inline void Set(Symbol symbol, const std::string& src)
-		{
-			type = TokenType::SYMBOL;
-			value = symbol;
+			type = TokenType::NAME;
 			source = src;
 		}
+	}
 
-		inline void Set(Reserved reserved, const std::string& src)
-		{
-			type = TokenType::RESERVED;
-			value = reserved;
-			source = src;
-		}
+	inline void Set(Symbol symbol, const std::string& src)
+	{
+		type = TokenType::SYMBOL;
+		value = symbol;
+		source = src;
+	}
 
-	};
+	inline void Set(Reserved reserved, const std::string& src)
+	{
+		type = TokenType::RESERVED;
+		value = reserved;
+		source = src;
+	}
+
+	static std::string Symbol2Str(Symbol symbol);
+
+	static std::string Reserved2Str(Reserved reserved);
+
+};
 }
 
 #endif //_TOKEN_HPP_
---
+++ src/Token/Token.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Katsu Matsuda on 2020/04/20.
+//
+
+#include "Token/Token.hpp"
+
+using namespace xylo;
+
+std::string Token::Symbol2Str(Symbol symbol)
+{
+	switch (symbol)
+	{
+	case Symbol::PLUS:
+		return "+";
+	case Symbol::MINUS:
+		return "-";
+	case Symbol::MUL:
+		return "*";
+	case Symbol::DIV:
+		return "/";
+	case Symbol::MOD:
+		return "%";
+	case Symbol::BIN_AND:
+		return "&";
+	case Symbol::BIN_OR:
+		return "|";
+	case Symbol::BIN_XOR:
+		return "^";
+	case Symbol::BIN_NOT:
+		return "~";
+	case Symbol::AND:
+		return "&&";
+	case Symbol::OR:
+		return "||";
+	case Symbol::NOT:
+		return "!";
+	case Symbol::EQUAL:
+		return "==";
+	case Symbol::NOT_EQUAL:
+		return "!=";
+	case Symbol::GREATER:
+		return ">";
+	case Symbol::GREATER_EQ:
+		return ">=";
+	case Symbol::LESS:
+		return "<";
+	case Symbol::LESS_EQ:
+		return "<=";
+	case Symbol::ASSIGN:
+		return "=";
+	case Symbol::QUE:
+		return "?";
+	case Symbol::COMMA:
+		return ",";
+	case Symbol::DOT:
+		return ".";
+	case Symbol::LPAREN:
+		return "(";
+	case Symbol::RPAREN:
+		return ")";
+	case Symbol::LBRACKET:
+		return "{";
+	case Symbol::RBRACKET:
+		return "}";
+	default:
+		return "";
+	}
+}
+
+std::string Token::Reserved2Str(Reserved reserved)
+{
+
+}
