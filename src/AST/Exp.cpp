8938d76d6ba6f758b7ac895a6b33b373bb9e2c3f
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-20 22:28:13 +0900

    Fix Exp::Code2Str()

--- src/AST/Exp.cpp
+++ src/AST/Exp.cpp
@@ -9,16 +9,16 @@ using namespace xylo::node;
 std::string Exp::Node2Str(uint64_t indent)
 {
 	if (std::holds_alternative<std::unique_ptr<Value>>(value))
-		std::get<std::unique_ptr<Value>>(value)->Node2Str(indent);
+		return std::get<std::unique_ptr<Value>>(value)->Node2Str(indent);
 
 	if (std::holds_alternative<std::unique_ptr<BOperator>>(value))
-		std::get<std::unique_ptr<Value>>(value)->Node2Str(indent);
+		return std::get<std::unique_ptr<BOperator>>(value)->Node2Str(indent);
 
 	if (std::holds_alternative<std::unique_ptr<UOperator>>(value))
-		std::get<std::unique_ptr<Value>>(value)->Node2Str(indent);
+		return std::get<std::unique_ptr<UOperator>>(value)->Node2Str(indent);
 
 	if (std::holds_alternative<std::unique_ptr<Func>>(value))
-		std::get<std::unique_ptr<Value>>(value)->Node2Str(indent);
+		return std::get<std::unique_ptr<Func>>(value)->Node2Str(indent);
 
 	return "";
 }
