c227399ffc2ecf57bbf951b04fa4424cb8db19b1
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-20 23:24:05 +0900

    Add CodeGen

---
+++ include/CodeGen/CodeGen.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Katsu Matsuda on 2020/04/20.
+//
+
+#ifndef _CODEGEN_HPP_
+#define _CODEGEN_HPP_
+
+namespace xylo
+{
+class CodeGen
+{
+
+};
+}
+
+#endif //_CODEGEN_HPP_
---
+++ src/CodeGen/CodeGen.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Katsu Matsuda on 2020/04/20.
+//
+
+#include "CodeGen/CodeGen.hpp"
+
