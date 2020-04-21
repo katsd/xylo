24751d255c7738ae2cc3e590c48505e36437cea4
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-21 19:10:17 +0900

    Update CodeGen

--- include/CodeGen/CodeGen.hpp
+++ include/CodeGen/CodeGen.hpp
@@ -20,6 +20,40 @@ class CodeGen
 
 	std::vector<uint64_t> code;
 
+	bool ConvertRoot(std::unique_ptr<node::Root> node, uint64_t scope_id);
+
+	bool ConvertStmt(std::unique_ptr<node::Stmt> node, uint64_t scope_id);
+
+	bool ConvertFuncDef(std::unique_ptr<node::FuncDef> node, uint64_t scope_id);
+
+	bool ConvertFunc(std::unique_ptr<node::Func> node, uint64_t scope_id);
+
+	bool ConvertBlock(std::unique_ptr<node::Block> node, uint64_t scope_id);
+
+	bool ConvertAssign(std::unique_ptr<node::Assign> node, uint64_t scope_id);
+
+	bool ConvertIf(std::unique_ptr<node::If> node, uint64_t scope_id);
+
+	bool ConvertRepeat(std::unique_ptr<node::Repeat> node, uint64_t scope_id);
+
+	bool ConvertFor(std::unique_ptr<node::For> node, uint64_t scope_id);
+
+	bool ConvertWhile(std::unique_ptr<node::While> node, uint64_t scope_id);
+
+	bool ConvertReturn(std::unique_ptr<node::Return> node, uint64_t scope_id);
+
+	bool ConvertExp(std::unique_ptr<node::Exp> node, uint64_t scope_id);
+
+	bool ConvertUOperator(std::unique_ptr<node::UOperator> node, uint64_t scope_id);
+
+	bool ConvertValue(std::unique_ptr<node::Value> node, uint64_t scope_id);
+
+	bool ConvertInt(std::unique_ptr<node::Int> node, uint64_t scope_id);
+
+	bool ConvertFloat(std::unique_ptr<node::Float> node, uint64_t scope_id);
+
+	bool ConvertString(std::unique_ptr<node::String> node, uint64_t scope_id);
+
  public:
 	std::vector<uint64_t> GenerateCode();
 
--- src/CodeGen/CodeGen.cpp
+++ src/CodeGen/CodeGen.cpp
@@ -12,3 +12,88 @@ std::vector<uint64_t> CodeGen::GenerateCode()
 
 	return code;
 }
+
+bool CodeGen::ConvertRoot(std::unique_ptr<node::Root> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertStmt(std::unique_ptr<node::Stmt> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertFuncDef(std::unique_ptr<node::FuncDef> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertFunc(std::unique_ptr<node::Func> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertBlock(std::unique_ptr<node::Block> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertAssign(std::unique_ptr<node::Assign> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertIf(std::unique_ptr<node::If> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertRepeat(std::unique_ptr<node::Repeat> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertFor(std::unique_ptr<node::For> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertWhile(std::unique_ptr<node::While> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertReturn(std::unique_ptr<node::Return> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertExp(std::unique_ptr<node::Exp> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertUOperator(std::unique_ptr<node::UOperator> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertValue(std::unique_ptr<node::Value> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertInt(std::unique_ptr<node::Int> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertFloat(std::unique_ptr<node::Float> node, uint64_t scope_id)
+{
+
+}
+
+bool CodeGen::ConvertString(std::unique_ptr<node::String> node, uint64_t scope_id)
+{
+
+}
