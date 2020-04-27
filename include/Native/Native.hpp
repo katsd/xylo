3a9ea3387e4864f65fea1ed660f843db0e163b04
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-27 23:11:43 +0900

    Add Native

---
+++ include/Native/Native.hpp
@@ -0,0 +1,42 @@
+//
+// Created by Katsu Matsuda on 2020/04/27.
+//
+
+#ifndef _NATIVE_HPP_
+#define _NATIVE_HPP_
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "VM/VM.hpp"
+
+namespace xylo::native
+{
+class Native
+{
+ private:
+	struct Func
+	{
+		vm::Obj (* func)(std::unique_ptr<vm::Obj[]>& args, uint64_t arg_num);
+
+		std::string func_name;
+
+		uint64_t arg_num;
+	};
+
+	static std::vector<Func> funcs;
+
+ public:
+	static void Init();
+
+	static void
+	AddFunc(vm::Obj (* func)(std::unique_ptr<vm::Obj[]>& args, uint64_t arg_num), std::string name, uint64_t arg_num);
+
+	static std::string StandardLibraryCode();
+
+	static vm::Obj Call(uint64_t func_id, std::unique_ptr<vm::Obj[]>& args);
+};
+}
+
+#endif //_NATIVE_HPP_
---
+++ src/Native/Native.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Katsu Matsuda on 2020/04/27.
+//
+
+#include "Native/Native.hpp"
+
+using namespace xylo;
+using namespace xylo::native;
+
+void Native::Init()
+{
+	funcs.clear();
+}
+
+void Native::AddFunc(vm::Obj (* func)(std::unique_ptr<vm::Obj[]>&, uint64_t), std::string name, uint64_t arg_num)
+{
+	funcs.push_back({ func, std::move(name), arg_num });
+}
+
+std::string Native::StandardLibraryCode()
+{
+	Init();
+
+	std::string code;
+
+	for (int i = 0; i < funcs.size(); i++)
+	{
+		auto func = funcs[i];
+		code += "@native(" + std::to_string(i) + ")\n";
+		code += "func " + func.func_name + "(";
+
+		for (int j = 0; j < func.arg_num; j++)
+		{
+			code += "_" + std::to_string(j);
+			if (j < func.arg_num - 1)
+				code += ",";
+		}
+
+		code += "){}\n";
+	}
+
+	return code;
+}
+
+vm::Obj Native::Call(uint64_t func_id, std::unique_ptr<vm::Obj[]>& args)
+{
+	auto func = funcs[func_id];
+	return func.func(args, func.arg_num);
+}
