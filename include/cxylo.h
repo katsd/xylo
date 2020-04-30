b26cd52c83bf08dea161e669d299b08e23366a7d
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-30 18:01:17 +0900

    Update cxylo.h

--- include/cxylo.h
+++ include/cxylo.h
@@ -5,11 +5,19 @@
 #ifndef _CXYLO_H_
 #define _CXYLO_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "xylo.hpp"
 
 typedef struct
 {
 
 } xylo;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //_CXYLO_H_
